705f7cb269971ac7b12ea261afdb62d65c90c870
jason-dever <jasonello22@gmail.com>
2026-06-22 16:40:19 -0400

    move sim circuit construction to its own fn

--- 1d-schrodinger/1d-solver.py
+++ 1d-schrodinger/1d-solver.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from math import ceil, floor
 from qiskit_ibm_runtime import QiskitRuntimeService
 from qiskit import QuantumCircuit
@@ -35,7 +36,7 @@ def kinetic(n, dt):
     return qc
 
 # The kinetic energy circuit is the same in all cases, so we don't need to let
-# it vary by passing it into the function.
+# it vary by passing it into this function.
 def get_one_iter(potential_qc, dt):
     qc = QuantumCircuit(num_qubits)
     qc.compose(kinetic(num_qubits, dt), inplace=True)
@@ -43,30 +44,36 @@ def get_one_iter(potential_qc, dt):
 
     return qc
 
-def sim(initial_statevector, potential_qc, dt, final_t, backend):
-    sim = QuantumCircuit(num_qubits)
-    sim.initialize(initial_statevector)
-
-    num_iter = 0 if final_t == 0 else floor(final_t/dt)
+def get_sim_circuit(potential_qc, dt, final_t):
+    qc = QuantumCircuit(num_qubits)
+    num_iter = floor(final_t/dt)
 
     # Since the operators for potential and kinetic energy do not commute
     # we apply the Trotter formula, taking timesteps of length dt from t=0 to t=final_t.
     for k in range(num_iter):
-        sim.compose(get_one_iter(potential_qc, dt), inplace=True)
+        qc.compose(get_one_iter(potential_qc, dt), inplace=True)
 
     # If final_t is not a multiple of dt, we iterate to the largest
     # time step before final_t in the loop above and step to final_t here.
-    sim.compose(get_one_iter(potential_qc, final_t-dt*num_iter), inplace=True)
+    qc.compose(get_one_iter(potential_qc, final_t-dt*num_iter), inplace=True)
+
+    return qc
+
+
+def sim(initial_statevector, potential_qc, dt, final_t, backend):
+    sim = QuantumCircuit(num_qubits)
+    sim.initialize(initial_statevector)
+    sim.compose(get_sim_circuit(potential_qc, dt, final_t), inline=True)
     sim.measure_all()
 
     pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
     isa_circuit = pm.run(sim)
 
     sampler = Sampler(mode=backend)
-    sampler.options.default_shots = 256 
+    sampler.options.default_shots = 512  
 
     job = sampler.run([isa_circuit])
-    # print(f"Job ID: {job.job_id()}")
+    print(f"Job ID: {job.job_id()}")
     counts = job.result()[0].data.meas.get_counts()
 
     # Fill counts with all possible measurement outcomes; by default 
@@ -80,8 +87,8 @@ def sim(initial_statevector, potential_qc, dt, final_t, backend):
 
 mu = 0
 sigma = 0.3
-momentum = 3
-dt = 0.01
+momentum = 0
+dt = 0.8
 
 x = np.linspace(-d, d, num=2**num_qubits)
 
@@ -89,7 +96,7 @@ psi_0 = np.exp(-(x - mu)**2 / (2 * sigma**2)) * np.exp(1j * momentum * x)
 psi_0 /= np.linalg.norm(psi_0)
 initial_statevector = Statevector(psi_0)
 
-for dt in [x/100 for x in range(25)]:
-    counts = sim(initial_statevector, QuantumCircuit(num_qubits), dt, dt, backend)
-    plot_histogram(counts, title=f"after time {dt}")
+for t in [x/20 for x in range(6)]:
+    counts = sim(initial_statevector, QuantumCircuit(num_qubits), dt, t, backend)
+    plot_histogram(counts, title=f"after time {t} (p=0)")
 plt.show()
