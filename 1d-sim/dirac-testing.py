09d59ed20132dfc7a8aed22b2c95da00ec8316f5
jason-dever <jasonello22@gmail.com>
2026-07-10 20:48:58 -0400

    add transport testing code

--- 1d-sim/dirac-testing.py
+++ 1d-sim/dirac-testing.py
@@ -1,19 +1,22 @@
 from sim_essentials import *
-from itertools import chain
 import dirac
 import matplotlib.pyplot as plt
 
 def gaussian(mu, sigma, momentum, x):
     return np.exp(-(x-mu)**2/(2*sigma**2)) * np.exp(1j*momentum*x)
 
-# detailed=0, total pass/fail + avg error, detailed=1 gives individual error for each iter, detailed=2 plots probabilities against ideal
-def test_linear_transport(grid: Grid, times, f, g, detailed=0, tolerance=1e-1):
-    # Here we test our implementation of the solution to the 1d linear transport equation
-    # partial_t phi_1 = -partial_x phi_1
-    # partial_t phi_2 = partial_x phi_2.
-    # The rest of Dirac for a free particle is just a few gates, and this in particular 
-    # is very easy to test against an analytical solution, simply given by translation.
+def transport_solution_1(f, g, x, t): # First spinor component
+    return 1/2*(f(x-t) + g(x-t) + f(x+t) - g(x+t))
 
+def transport_solution_2(f, g, x, t): # Second component
+    return 1/2*(f(x-t) + g(x-t) - f(x+t) + g(x+t))
+
+# Here we test our implementation of the solution to the transport term
+# partial_t psi = -partial_x sigma_x psi
+# The justification for the analytical solution is given in dirac.tex.
+# detailed=0 gives pass/fail + avg error, detailed=1 gives individual 
+# error for each iter, detailed=2 plots probabilities against ideal
+def test_transport(grid: Grid, times, f, g, detailed=0, tolerance=1e-4):
     errors = []
 
     initial_psi = np.concatenate((f(grid.x)*grid.fftshift_correction, g(grid.x)*grid.fftshift_correction))
@@ -21,33 +24,28 @@ def test_linear_transport(grid: Grid, times, f, g, detailed=0, tolerance=1e-1):
     for t in times:
         qc = get_empty_sim(grid, num_spinor=1)
         qc.initialize(Statevector(initial_psi))
-
-        # dirac.transport() implements a sigma_x term by diagonalizing sigma_x = H sigma_z H.
-        # In this test, we are concerned with the already diagonal linear transport equation,
-        # so we remove the Hadamard gates at the start and end of the circuit, noting that H^-1 = H.
-        indicator_idx = qc.num_qubits-1
-        qc.h(indicator_idx)
         qc.compose(dirac.transport(grid, t), inplace=True)
-        qc.h(indicator_idx)
 
-        ideal_psi = np.concatenate((f(grid.x - t)*grid.fftshift_correction, g(grid.x + t)*grid.fftshift_correction))
+        ideal_psi = np.concatenate((transport_solution_1(f, g, grid.x, t)*grid.fftshift_correction, 
+                                    transport_solution_2(f, g, grid.x, t)*grid.fftshift_correction))
         ideal_psi /= np.linalg.norm(ideal_psi)
-
         final_statevector = Statevector.from_circuit(qc)
-        error = np.linalg.norm(ideal_psi - final_statevector.data)
+
+        error =  abs(1-abs(final_statevector.inner(Statevector(ideal_psi))))
         errors.append(error)
 
         if detailed:
-            print(f"linear transport, t={t}, error={error}")
+            print(f"transport, t={t}, error={error}")
         if detailed == 2:
-            num_pts = 512
-            x_fine = np.linspace(-grid.d, grid.d, num_pts, endpoint=False)
-            ideal_curve = abs(f(x_fine-t)**2) + abs(g(x_fine+t))**2
+            num_pts = 1024
+            x_fine = np.linspace(-grid.d, grid.d, num_pts)    
+            ideal_curve = abs(transport_solution_1(f, g, x_fine, t))**2 + abs(transport_solution_2(f, g, x_fine, t))**2
             ideal_curve /= ideal_curve.sum()
+
             plt.plot(x_fine, ideal_curve*num_pts/grid.N, "r-")
             plt.bar(grid.x, final_statevector.probabilities(qargs=list(range(grid.num_qubits))), width=0.7*grid.dx)
 
-    print(f"linear transport average error {sum(errors)/len(errors)}", end=", ")
+    print(f"transport average error {sum(errors)/len(errors)}", end=", ")
     if max(errors) < tolerance:
         print("pass")
     else:
@@ -55,6 +53,6 @@ def test_linear_transport(grid: Grid, times, f, g, detailed=0, tolerance=1e-1):
 
     
 grid = Grid(num_qubits = 7, d=6*np.pi)
-test_linear_transport(grid, [4*t for t in range(4)], lambda x: gaussian(-1, 1, 1, x), lambda x: gaussian(-1, 1/np.sqrt(2), -1, x), detailed=2)
+test_transport(grid, [4*t for t in range(4)], lambda x: gaussian(0, 1, 0, x), lambda x: 0, detailed=2)
 
 plt.show()
